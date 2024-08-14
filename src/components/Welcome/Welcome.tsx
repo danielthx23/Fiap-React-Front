ec1dd0928591939a511cb6c915fb3aa75ed6bd9e
Daniel Akiyama <danielakiyama8@gmail.com>
2024-08-13 20:19:05 -0400

    Fix: Adicionei o RM que estava faltando, e arrumei as cores dos botões

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -7,7 +7,7 @@ function Welcome() {
       <svg className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000e12" fill-opacity="1" d="M0,192L80,181.3C160,171,320,149,480,170.7C640,192,800,256,960,261.3C1120,267,1280,213,1360,186.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
       <div className="container-text">
         <h1 className="titulo">Bem-vindo a minha prática de React!</h1>
-        <h3 className="autor">Daniel Saburo Akiyama</h3>
+        <h3 className="autor">Daniel Saburo Akiyama RM 558263</h3>
         <h2 className="subtitulo">Esta prática consiste em: </h2>
       </div>
         <div className="lista-cards">
